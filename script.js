e5985efe68636fa57c89d2d7dee39d4284ddab1d
Dylan <dylant7192@gmail.com>
2024-11-14 10:40:55 +0000

    Added form for new pushes to word arrays

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ nouns = ["dog", "cat", "car", "tree", "house", "river", "mountain", "book", "com
 verbs = ["runs", "jumps", "swims", "flies", "drives", "writes", "reads", "sings", "dances", "eats", "drinks", "sleeps", "talks", "listens", "watches", "plays", "works", "studies", "laughs", "cries"];
 adverbs = ["quickly", "slowly", "loudly", "quietly", "happily", "sadly", "angrily", "eagerly", "bravely", "carefully", "recklessly", "gently", "roughly", "kindly", "cruelly", "funnily", "seriously", "brightly", "darkly", "sharply"];
 
-function RandomIndex(ceiling) {
-    if (!Number.isInteger(ceiling)) {alert("Non-integer made its way into the system!"); return;}
-    
-    const max = Math.floor(ceiling);
-    return Math.floor(Math.random() * max)
-}
-
 let btn = document.getElementById("btn-gen");
 let text = document.getElementById("text");
 let div = document.getElementById("div-btn-container");
+let adj_in = document.getElementById("adjective-input");
+let noun_in = document.getElementById("noun-input");
+let verb_in = document.getElementById("verb-input");
+let adv_in = document.getElementById("adverb-input");
+let submit = document.getElementById("submit");
 
 btn.addEventListener("click", () => {
     text.innerHTML = "The " + adjectives[RandomIndex(adjectives.length)] + " " + nouns[RandomIndex(nouns.length)] + " " + verbs[RandomIndex(verbs.length)] + " " + adverbs[RandomIndex(adverbs.length)] + ".";
@@ -22,3 +20,21 @@ btn.addEventListener("click", () => {
     text.style.left = w_offset + "px"
     text.style.top = h_offset + "px"
 })
+
+submit.addEventListener("click", () => {
+    if (adj_in.value != "") {adjectives.push(adj_in.value);}
+    if (noun_in.value != "") {nouns.push(noun_in.value);}
+    if (verb_in.value != "") {verbs.push(verb_in.value);}
+    if (adv_in.value != "") {adverbs.push(adv_in.value);}
+    adj_in.value = "";
+    noun_in.value = "";
+    verb_in.value = "";
+    adv_in.value = "";
+})
+
+function RandomIndex(ceiling) {
+    if (!Number.isInteger(ceiling)) {alert("Non-integer made its way into the system!"); return;}
+    
+    const max = Math.floor(ceiling);
+    return Math.floor(Math.random() * max)
+}
